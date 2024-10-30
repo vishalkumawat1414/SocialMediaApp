4a9302ab30c185287be1b3738b714d580712a712
vishalkumawat1414 <vishal1414kumawat@email.com>
2024-10-30 17:03:01 +0530

    ds

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(
 	cors({
 		credentials: true,
-		origin: "http://localhost:3000",
+		origin: "https://socialmediaapp-kappa.vercel.app/#/login",
 		optionSuccessStatus: 200,
 	}),
 );
